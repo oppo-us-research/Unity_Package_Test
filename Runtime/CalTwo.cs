2d7228dda1a7df26f9d5fa03a8c1ec1d4961bf21
oppo-us-research <92191874+oppo-us-research@users.noreply.github.com>
2024-09-18 10:18:19 -0700

    Update version to 1.0.1

---
+++ Runtime/CalTwo.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CalTwo : MonoBehaviour
+{
+    public static int Add(int a, int b)
+    {
+        return a + b;
+    }
+}
